01ba4760195978a834732bbee09851268ec91613
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-10 19:36:52 +0200

    Change languages logic implemented

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,5 +1,23 @@
+import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
+import { languages } from '../../../utils/languages';
+
+const number: number = 15;
+
 const Login = () => {
-  return <div>Login</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <p>{t('login')}</p>
+      <p>{t('days_since_implementation', { number })}</p>
+      {languages.map(({ code, name, country_code }) => (
+        <button key={country_code} onClick={() => i18next.changeLanguage(code)}>
+          {name} <span className={`fi fi-${country_code} fis`}></span>
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Login;
