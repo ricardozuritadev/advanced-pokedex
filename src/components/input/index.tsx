729a51d3a7516448c33b816ca6880ea81a3f4960
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-11 14:23:10 +0200

    Input component created

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,15 @@
-const Input = () => {
-  return <div>Input</div>;
+import { InputProps } from './types';
+
+const Input: React.FC<InputProps> = ({ position, small, ...props }) => {
+  return (
+    <section
+      className={`input ${position && `input--${position}`} ${
+        small && `input--small`
+      }`}
+    >
+      <input className="input__text" {...props} />
+    </section>
+  );
 };
 
 export default Input;
