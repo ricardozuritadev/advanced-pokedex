32aad3f26f1f57c7edec0066f7c63428a7b3cb93
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-11 14:23:38 +0200

    Register component created

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,5 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import Input from '../../../components/input';
+import Button from '../../../components/button';
+
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const handleClick = () => navigate('/login');
+
+  return (
+    <section>
+      <section>
+        <img src="" alt="" />
+      </section>
+
+      <section>
+        <Input type="text" placeholder="email" />
+        <Input type="text" placeholder="username" />
+        <Input type="password" placeholder="password" />
+      </section>
+
+      <section>
+        <Button text="Sign up" handleClick={} />
+        <p>Already have an account?</p>
+        <p onClick={handleClick}>Log in here</p>
+      </section>
+    </section>
+  );
 };
 
 export default Register;
