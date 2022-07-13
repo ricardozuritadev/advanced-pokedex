4d81a10e2e0056ad82164063707d03da915bad50
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-13 20:34:38 +0200

    chore: implemented imgLoader function in utils

---
+++ src/utils/imgLoader.ts
@@ -0,0 +1,9 @@
+import maleTrainer from '../assets/images/male-trainer.png';
+import femaleTrainer from '../assets/images/female-trainer.png';
+
+const images = {
+  maleTrainer,
+  femaleTrainer,
+};
+
+export default images;
